62e4db2104effefd1fe5cb863e837e1091cec222
hishamrehab <hishamrehab309@gmail.com>
2024-03-11 17:54:44 +0200

    add porfofilo card

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -7,6 +7,7 @@ import logo from "../../images/logo.png";
 import gallery from "../../images/gallery-3 (1).jpg";
 import work from "../../images/work-2.jpg";
 import heroPanner from "../../images/hero-banner (1).png";
+import portifolio from "../../images/hishamrehab.png";
 const Main = () => {
   return (
     <main>
@@ -30,47 +31,59 @@ const Main = () => {
 
       {/* { imgUrl, title, subTitle, githubLink, liveDemo } */}
       {/* ================================================= */}
-      <section className="flex right-section">
-        <Card
-          imgUrl={blog}
-          title="Ecommerce Website"
-          subTitle="Utilizing React , CSS  , Bootstrap , Tailwind , Material-UI (MUI)  ,Axios , Axios , •Redux Toolkit , SCSS and Swiper and deploy the project on Vercel.  "
-          githubLink="https://github.com/hishamrehab/EcommerceWebsiteWithReduxToolkit"
-          liveDemo="https://ecommerce-react--reduxtoolkit.web.app/"
-        />
 
-        <Card
-          imgUrl={gallery}
-          title="Tours Website"
-          subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
-          githubLink="https://github.com/hishamrehab/Tours-Website-React-js"
-          liveDemo="https://tours-website-react-js.vercel.app/"
-        />
 
-        <Card
-          imgUrl={logo}
-          title="YouTube Clone"
-          subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
-          githubLink="https://github.com/hishamrehab/YouTube"
-          liveDemo="https://you-tube-nzf7.vercel.app/"
-        />
+      
+        <section className="flex right-section">
+          <Card
+            imgUrl={blog}
+            title="Ecommerce Website"
+            subTitle="Utilizing React , CSS  , Bootstrap , Tailwind , Material-UI (MUI)  ,Axios , Axios , •Redux Toolkit , SCSS and Swiper and deploy the project on Vercel.  "
+            githubLink="https://github.com/hishamrehab/EcommerceWebsiteWithReduxToolkit"
+            liveDemo="https://ecommerce-react--reduxtoolkit.web.app/"
+          />
 
-        <Card
-          imgUrl={work}
-          title="Cars Store"
-          subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
-          githubLink="https://github.com/hishamrehab/carsStore"
-          liveDemo="https://cars-store-omega.vercel.app/"
-        />
+          <Card
+            imgUrl={gallery}
+            title="Tours Website"
+            subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
+            githubLink="https://github.com/hishamrehab/Tours-Website-React-js"
+            liveDemo="https://tours-website-react-js.vercel.app/"
+          />
 
-        <Card
-          imgUrl={heroPanner}
-          title="Education Website"
-          subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
-          githubLink="https://github.com/hishamrehab/Education-Website"
-          liveDemo="https://education-website-black.vercel.app/"
-        />
-      </section>
+          <Card
+            imgUrl={logo}
+            title="YouTube Clone"
+            subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
+            githubLink="https://github.com/hishamrehab/YouTube"
+            liveDemo="https://you-tube-nzf7.vercel.app/"
+          />
+
+          <Card
+            imgUrl={work}
+            title="Cars Store"
+            subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
+            githubLink="https://github.com/hishamrehab/carsStore"
+            liveDemo="https://cars-store-omega.vercel.app/"
+          />
+
+          <Card
+            imgUrl={heroPanner}
+            title="Education Website"
+            subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
+            githubLink="https://github.com/hishamrehab/Education-Website"
+            liveDemo="https://education-website-black.vercel.app/"
+          />
+
+          <Card
+            imgUrl={portifolio}
+            title="portifolio Page"
+            subTitle=" Utilizing React js ,Css , Vite retrieval for an exceptional user experience.and deploy the project on Firebase."
+            githubLink="https://github.com/hishamrehab/Portlifolio-Page"
+            liveDemo="https://portifolio-fd23c.web.app/"
+          />
+        </section>
+    
     </main>
   );
 };
