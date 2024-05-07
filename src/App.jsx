44fa6085d29f44cce49a748e474b85acdf060df4
hishamrehab <hishamrehab309@gmail.com>
2024-05-08 02:05:44 +0300

    Add MOre Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import { useEffect, useState } from "react";
 import Header from "./components/1-header/header";
 import Hero from "./components/2-hero/Hero";
-import Main from "./components/3-main/Main";
+
 import Contact from "./components/4-contact/Contact";
 import Footer from "./components/5-footer/footer";
 import { BsArrowUpCircleFill } from "react-icons/bs";
 import { AiFillFunnelPlot } from "react-icons/ai";
-function App() {
-     
-  useEffect (() => {
-   {window.addEventListener("scroll" , () => {
-    if (window.scrollY > 300) {
-      setShowScrollBtn(true)
-    } else {
-      setShowScrollBtn(false)
-    }
-   })}
-  } , [])
-  const [showScrollBtn , setShowScrollBtn] = useState(false)
 
+import Main from "./components/3-main/Main";
+function App() {
+  useEffect(() => {
+    {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+          setShowScrollBtn(true);
+        } else {
+          setShowScrollBtn(false);
+        }
+      });
+    }
+  }, []);
+  const [showScrollBtn, setShowScrollBtn] = useState(false);
 
   return (
     <div id="up" className="container">
       <Header />
       <Hero />
+
       <div className="divider" />
       <Main />
       <div className="divider" />
--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import "./Main.css";
 import Card from "./Card";
-
 import blog from "../../images/blog-4.jpg";
 import logo from "../../images/logo.png";
 import gallery from "../../images/gallery-3 (1).jpg";
 import work from "../../images/work-2.jpg";
 import heroPanner from "../../images/hero-banner (1).png";
 import portifolio from "../../images/hishamrehab.png";
+import bg from "../../images/bg.jpg";
+import Image4 from "../../images/image4.jpeg";
+
 const Main = () => {
   return (
     <main>
@@ -32,58 +33,71 @@ const Main = () => {
       {/* { imgUrl, title, subTitle, githubLink, liveDemo } */}
       {/* ================================================= */}
 
+      <section className="flex right-section">
+        <Card
+          imgUrl={blog}
+          title="Ecommerce Website"
+          subTitle="Utilizing React , CSS  , Bootstrap , Tailwind , Material-UI (MUI)  ,Axios , Axios , •Redux Toolkit , SCSS and Swiper and deploy the project on Vercel.  "
+          githubLink="https://github.com/hishamrehab/EcommerceWebsiteWithReduxToolkit"
+          liveDemo="https://ecommerce-react--reduxtoolkit.web.app/"
+        />
 
-      
-        <section className="flex right-section">
-          <Card
-            imgUrl={blog}
-            title="Ecommerce Website"
-            subTitle="Utilizing React , CSS  , Bootstrap , Tailwind , Material-UI (MUI)  ,Axios , Axios , •Redux Toolkit , SCSS and Swiper and deploy the project on Vercel.  "
-            githubLink="https://github.com/hishamrehab/EcommerceWebsiteWithReduxToolkit"
-            liveDemo="https://ecommerce-react--reduxtoolkit.web.app/"
-          />
+        <Card
+          imgUrl={gallery}
+          title="Tours Website"
+          subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
+          githubLink="https://github.com/hishamrehab/Tours-Website-React-js"
+          liveDemo="https://tours-website-react-js.vercel.app/"
+        />
 
-          <Card
-            imgUrl={gallery}
-            title="Tours Website"
-            subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
-            githubLink="https://github.com/hishamrehab/Tours-Website-React-js"
-            liveDemo="https://tours-website-react-js.vercel.app/"
-          />
+        <Card
+          imgUrl={logo}
+          title="YouTube Clone"
+          subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
+          githubLink="https://github.com/hishamrehab/YouTube"
+          liveDemo="https://you-tube-nzf7.vercel.app/"
+        />
 
-          <Card
-            imgUrl={logo}
-            title="YouTube Clone"
-            subTitle="Utilizing Seamlessly integrated React Router, Material-UI (MUI) ,MUI Icons and Axios to optimize navigation and streamline API data retrieval for an exceptional user experience.deploy the project on Vercel.  "
-            githubLink="https://github.com/hishamrehab/YouTube"
-            liveDemo="https://you-tube-nzf7.vercel.app/"
-          />
+        <Card
+          imgUrl={work}
+          title="Cars Store"
+          subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
+          githubLink="https://github.com/hishamrehab/carsStore"
+          liveDemo="https://cars-store-omega.vercel.app/"
+        />
 
-          <Card
-            imgUrl={work}
-            title="Cars Store"
-            subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
-            githubLink="https://github.com/hishamrehab/carsStore"
-            liveDemo="https://cars-store-omega.vercel.app/"
-          />
+        <Card
+          imgUrl={heroPanner}
+          title="Education Website"
+          subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
+          githubLink="https://github.com/hishamrehab/Education-Website"
+          liveDemo="https://education-website-black.vercel.app/"
+        />
 
-          <Card
-            imgUrl={heroPanner}
-            title="Education Website"
-            subTitle=" Utilizing HTML, CSS, JavaScript, Bootstrap, Font Awesome, AOS, and Tailwind CSS , Jquery , FirebaseSeam (MUIdata retrievalce) and deploy the project on Vercel."
-            githubLink="https://github.com/hishamrehab/Education-Website"
-            liveDemo="https://education-website-black.vercel.app/"
-          />
+        <Card
+          imgUrl={portifolio}
+          title="portifolio Page"
+          subTitle=" Utilizing React js ,Css , Vite retrieval for an exceptional user experience.and deploy the project on Firebase."
+          githubLink="https://github.com/hishamrehab/Portlifolio-Page"
+          liveDemo="https://portifolio-fd23c.web.app/"
+        />
 
-          <Card
-            imgUrl={portifolio}
-            title="portifolio Page"
-            subTitle=" Utilizing React js ,Css , Vite retrieval for an exceptional user experience.and deploy the project on Firebase."
-            githubLink="https://github.com/hishamrehab/Portlifolio-Page"
-            liveDemo="https://portifolio-fd23c.web.app/"
-          />
-        </section>
-    
+        <Card
+          imgUrl={bg}
+          title="weather app"
+          subTitle=" Utilizing React Js , CSS and Axios Library. for an exceptional user experience.and deploy the project on Vercel."
+          githubLink="https://github.com/hishamrehab/Weather-App-React-Js"
+          liveDemo="https://weather-app-react-js-ruddy.vercel.app/"
+        />
+
+        <Card
+          imgUrl={Image4}
+          title="Restaurant Website "
+          subTitle=" Utilizing React , Sass ,Internal Data File : Leveraged internal data files for efficient storage and Progressive Web Apps (PWAs) .and deploy the project on Vercel."
+          githubLink="https://github.com/hishamrehab/Rolls-restaurant"
+          liveDemo="https://rolls-restaurant.vercel.app/"
+        />
+      </section>
     </main>
   );
 };
