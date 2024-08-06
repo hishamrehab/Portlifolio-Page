8a167124d253d72fcb86f39669ac419280f0a71f
My Name <hishamrehab309@gmail.com>
2024-08-06 17:34:23 +0300

    Edit more link

--- src/components/3-main/Card.jsx
+++ src/components/3-main/Card.jsx
@@ -5,33 +5,37 @@ import { AiFillGithub, AiOutlineArrowRight } from "react-icons/ai";
 import "./Main.css";
 const Card = ({ imgUrl, title, subTitle, githubLink, liveDemo }) => {
   return (
-   
-      <article className="card">
-        <img src={imgUrl} width={"266px"} alt="" />
-        <div style={{ width: "266px" }} className="box">
-          <h1 className="hello" alt="">
-            {title}
-          </h1>
-          <p className="sub-title">{subTitle}</p>
-          <div className="flex icons">
-            <div style={{ gap: "17px" }}>
-              <a className="icon-link" href={liveDemo}>
-                <BsLink45Deg />
-              </a>
-              <a className="icon-github" href={githubLink}>
-                <AiFillGithub />
-              </a>
-            </div>
-            <a className="link flex" href="" style={{ alignSelf: "end" }}>
-              more
-              <span>
-                <AiOutlineArrowRight />
-              </span>
+
+    <article className="card">
+      <img src={imgUrl} width={"266px"} alt="" />
+      <div style={{ width: "266px" }} className="box">
+        <h1 className="hello" alt="">
+          {title}
+        </h1>
+        <p className="sub-title">{subTitle}</p>
+        <div className="flex icons">
+          <div style={{ gap: "17px" }}>
+            <a className="icon-link" href={liveDemo}>
+              <BsLink45Deg />
+            </a>
+            <a className="icon-github" href={githubLink}>
+              <AiFillGithub />
             </a>
           </div>
+          <div className="more-link">
+            <a className="link flex" href="" style={{ alignSelf: "end" }}>
+              more
+
+              <AiOutlineArrowRight className="arrow" />
+
+            </a>
+          </div>
+
+
         </div>
-      </article>
-   
+      </div>
+    </article >
+
   );
 };
 
